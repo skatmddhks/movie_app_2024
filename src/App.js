bd25519bf015ea5d178bdef07efa84051c433e01
tmddhksska <seung3280@gmail.com>
2024-06-24 21:59:49 +0900

    6/24 css 추가

--- src/App.js
+++ src/App.js
@@ -1,56 +1,56 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import './App.css';
 
-class App extends React.Component{
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
 
-  constructor(props) {
-    super(props);
-    console.log('hello');
+  componentDidMount() {
+    // 영화 데이터 로딩
+    this.getMovies();
   }
 
-  
-  state ={
-    count: 0,
+  // async랑 await은 짝꿍
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
 
-  }
-
-  add = () => {
-    this.setState(current =>({count: this.state.count +1 }));
-  }
-
-  minus = () => {
-    this.setState(currnet =>({count: this.state.count -1 }));
-  }
-
-
-
-  componentDidMount(){
-    console.log("component rendered");
-  }
-
-  componentDidUpdate(){
-    console.log("I just updated");
-  }
-
-  componentWillUnmount(){
-    console.log("Goodby, ruel world");
-  }
-
-
-  render () {
-    console.log('render');
+  render() {
+    const { isLoading, movies } = this.state;
     return (
-    <div>
-      <h1> The numver is: {this.state.count} </h1>
-      <button onClick={this.add}> Add </button>
-      <button onClick={this.minus}> Minus </button>
-    </div>
-    )
-
-
-    
+      <section class="container">
+        {isLoading? (
+          <div class="loader">
+            <span class="loader__text">"Loading...":</span>
+            </div> 
+            ): (
+            <div class="movies">
+            {
+              movies.map((movie) => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              ))}
+            </div>
+            )}
+      </section>
+      )
   }
-
-
 }
-
 export default App;
